7f5f496ae831fbcaa4317a27a7e5fb064d7bd656
Lucy Lin <Lucy@cardinalblue.com>
2014-01-17 13:28:00 +0800

    init

--- gen/jt/phoenix/android_deblur/R.java
+++ gen/jt/phoenix/android_deblur/R.java
@@ -24,17 +24,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int orien_values0=0x7f080003;
-        public static final int orien_values1=0x7f080004;
-        public static final int orien_values2=0x7f080005;
-        public static final int textView=0x7f080006;
-        public static final int values0=0x7f080000;
-        public static final int values1=0x7f080001;
-        public static final int values2=0x7f080002;
+        public static final int action_settings=0x7f08000e;
+        public static final int button1=0x7f08000a;
+        public static final int buttonClick=0x7f08000d;
+        public static final int cameraSurfaceView=0x7f08000c;
+        public static final int frameLayout1=0x7f08000b;
+        public static final int gyro_values0=0x7f080007;
+        public static final int gyro_values1=0x7f080008;
+        public static final int gyro_values2=0x7f080009;
+        public static final int orien_values0=0x7f080004;
+        public static final int orien_values1=0x7f080005;
+        public static final int orien_values2=0x7f080006;
+        public static final int textView1=0x7f080000;
+        public static final int values0=0x7f080001;
+        public static final int values1=0x7f080002;
+        public static final int values2=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int camera=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
